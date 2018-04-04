6dc0d1f265b30a99dd76ac9f65a43d9f1a4d1784
Dzenana Sabovic <dsabovic1@etf.unsa.ba>
2018-04-04 07:54:25 +0200

    Dodan adaptivni layout za administaciju

--- Projekat/Vicinor/Vicinor/App.xaml.cs
+++ Projekat/Vicinor/Vicinor/App.xaml.cs
@@ -73,7 +73,7 @@ namespace Vicinor
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(AdminUserOverview), e.Arguments);
+                    rootFrame.Navigate(typeof(AdminUpdate), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
